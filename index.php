19b8a287dd2fc2c4f9ed797bb6bc56e5628fcc44
Iñigo In The Cloud <gestudio@users.noreply.github.com>
2021-03-31 12:18:03 +0200

    Gestudio landing page

---
+++ index.php
@@ -0,0 +1,82 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html> 
+
+<head>
+	<title>Gestudio Cloud - Cloud Software Experts</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com"> 
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap" rel="stylesheet">
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
+
+	<style>
+		html,body{margin:0 auto}a,a:link,a:hover,a:visited{text-decoration:none}
+		body{min-width:400px;font-family: 'Noto Sans JP', sans-serif;overflow:hidden}
+		#intro{width:100%;background-color:#fff;height:4000px}
+		#left-picture{width:2%;height:100vh!important;position:relative;float:left;background-color:#0088cc}
+		p.big{font-weight:800;font-size:60px;color:#3e3f43;text-align:right;line-height:80px;margin-right:10px;margin-bottom:-37px}
+		span.big{background-color:#fff;padding:0 25px 10px}
+		#right-content{width:95%;position:relative;float:right}
+		#logo2{position: fixed;bottom: 2%;right: 1%;}
+		.button{font-size:20px;font-weight:700;color:#fff;line-height:50px;letter-spacing:0;position:relative;text-align:center;border:4px solid #fff;width:190px;height:50px;text-transform:uppercase;-o-transition:.2s;-ms-transition:.2s;-moz-transition:.2s;-webkit-transition:.2s;transition:.2s}
+		.button:hover{background-color:#fff;color:#0088cc}
+		p.header{font-weight:300;font-size:24px;color:#0088cc;text-align:left}
+		p.header span{color: #fff;background:#0088cc;padding:0px 5px;}
+		.bigEntrance{overflow:auto;}
+		#half{width:50%;height:100vh;position:relative;float:left}
+		.round-it-med{font-size:18px;font-weight:300;color:#fff;line-height:43px;letter-spacing:0;position:relative;float:left;text-align:center;border:1px solid #0088cc;border-radius:25px;width:auto;height:45px;background-color:#0088cc;-o-transition:.2s;-ms-transition:.2s;-moz-transition:.2s;-webkit-transition:.2s;transition:.2s;padding:0 30px;margin:1% 3%;}
+		.round-it-med:hover{background-color:#fff;color:#0088cc;border:1px solid #0088cc}
+		.round-it-social{font-size:18px;font-weight:300;color:#0088cc;line-height:43px;letter-spacing:0;position:relative;float:left;text-align:center;border:1px solid #0088cc;border-radius:25px;width:auto;height:45px;margin-left:5px;-o-transition:.2s;-ms-transition:.2s;-moz-transition:.2s;-webkit-transition:.2s;transition:.2s;padding:0 10px;margin:1% 3%;}
+		.round-it-social:hover{background-color:#0088cc;color:#fff;border:1px solid #0088cc}
+		hr{display:block;clear:both;border:0;border-bottom:1px solid #0088cc; margin: 5% 1%;}
+	</style>
+
+
+
+</head>
+<body>
+
+  
+<div id="intro">
+
+      <div id="left-picture">
+          
+		  
+      </div>
+      <div id="right-content">
+          <div id="text" style="padding: 0 10%;overflow:auto;">
+			  
+              <p class="header">
+				  <img src="apple-touch-icon.png" alt="" width="20" /> Gestudio Cloud <span>web services</span>
+			  </p>
+			  <h1>Cloud software development experts.</h1>
+              <div class="bigEntrance"><a class="round-it-med" href="https://webmail.x.gestudio.com"><i class="fa fa-envelope"></i> Access the Webmail</a></div> 
+				  
+			  <hr />
+			  <h3>Our products:</h3>
+			  <div class="bigEntrance">
+				  <a class="round-it-social" href="https://www.contasigma.com"><i class="fa fa-euro"></i> Contasigma</a> 
+				  <a class="round-it-social" href="https://www.flylogs.com"><i class="fa fa-plane"></i> Flylogs</a>
+				  <a class="round-it-social" href="https://www.non.eus"><i class="fa fa-map-marker"></i> Non</a>
+			  </div> 
+			  
+			
+		  
+			  <div id="logo2"><small>Gestudio Cloud LLC.</small> <img width="100" alt="gestudio cloud" src="gestudio-cloud.png"/></div>
+      </div><!--/right-content-->
+
+</div><!--/intro-->
+
+	  
+
+
+<script type="text/javascript">	  
+	  
+	
+</script>
+  
+  
+</body>
+</html>
